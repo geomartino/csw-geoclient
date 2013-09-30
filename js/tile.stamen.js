3561dae57379e309d429ebfd99cf45043f37b847
geomartino <geomartino@gmail.com>
2013-09-29 23:51:48 -0400

    cleanup

--- js/tile.stamen.js
+++ js/tile.stamen.js
@@ -38,10 +38,10 @@ var SUBDOMAINS = " a. b. c. d.".split(" "),
         }
     },
     ATTRIBUTION = [
-        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
-        'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+        'Map tiles by <a target="_new" href="http://stamen.com">Stamen Design</a>, ',
+        'under <a target="_new" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
+        'Data by <a target="_new" href="http://openstreetmap.org">OpenStreetMap</a>, ',
+        'under <a target="_new" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
     ].join("");
 
 // set up toner and terrain flavors
